401bd13b00ca283afcfaeba431ed1212ecf79371
Taha <tahazairig1@gmail.com>
2023-06-16 20:38:31 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints Numberz from 1 to 10.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('i');
+	}
+	putchar('\n');
+	return (0);
+}
