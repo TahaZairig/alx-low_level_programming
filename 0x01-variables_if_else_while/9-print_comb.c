85a7b37a60047bd4f4cc4112faff75a3538ce825
Taha <tahazairig1@gmail.com>
2023-06-16 22:41:38 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,14 @@
 int main(void)
 {
 	int i;
-	int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		putchar('0' + j);
+		if (i < 9)
 		{
-			putchar('0' + j);
-			putchar('0' + i);
-			putchar(', ');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
