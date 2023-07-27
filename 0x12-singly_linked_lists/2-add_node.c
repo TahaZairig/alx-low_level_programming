7b247f8dc7439d032391c3353771482f6c636981
Taha Zairig <zairigtaha1@gmail.com>
2023-07-27 13:06:10 -0700

    3. Add node at the end

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,11 +16,11 @@ list_t *add_node(list_t **head, const char *str)
 	unsigned int len = 0;
 
 	while (str[len])
-	len++;
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
-	return (NULL);
+		return (NULL);
 
 	new->str = strdup(str);
 	new->len = len;
