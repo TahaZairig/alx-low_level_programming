4a37ba63f6deaf6c80a38b0aaa210d400373b22a
Taha Zairig <zairigtaha1@gmail.com>
2023-08-03 12:26:41 -0700

    4. 100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - to set a value of the given bit to 0
+ * @n: pointer to the number to change
+ * @index: index of the bit to clear
+ *
+ * Return: 1 for success and -1 for failure
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index > 63)
+		return (-1);
+
+	*n = (~(1UL << index) & *n);
+	return (1);
+
+}
+
